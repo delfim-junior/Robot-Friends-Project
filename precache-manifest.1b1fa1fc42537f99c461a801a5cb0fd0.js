c126bbf58feedf2212f0b27c0f1f02612ade8dc0
delfim-junior <delfim.uu.junior@gmail.com>
2020-02-27 19:28:43 +0200

    Updates

---
+++ precache-manifest.1b1fa1fc42537f99c461a801a5cb0fd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44aca8482c2a9a0c2bbbb67c0a2c44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6949092d7972ec9a9efb",
+    "url": "/static/css/main.230abcc0.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6949092d7972ec9a9efb",
+    "url": "/static/js/main.082bb487.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
